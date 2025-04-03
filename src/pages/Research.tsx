65ede08e0e6f647937cf56066bf04748f063d016
Planva <planvaofficial@gmail.com>
2025-04-03 14:57:33 +0800

    xfurl

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -27,7 +27,7 @@ export function Research() {
 
             <div className="flex justify-center my-8">
               <Link
-                to="/test"
+                to="/tat-test-online"
                 className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Take the TAT Test
